459ddb46c10ac2a63bd02e2f91d8c433320187c7
webdevsyed <118727046+webdevsyed@users.noreply.github.com>
2023-04-10 01:35:10 +0530

    Homepage css

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,34 @@
-import { Outlet , useOutletContext } from "react-router-dom";
+import { Outlet, useOutletContext } from "react-router-dom";
 
 // import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 
+const Section = ({title,desc}) => {
+    return (
+        <div className="border border-gray-600 p-5">
+            <h1 className="font-bold text-xl">{title}</h1>
+            <p className="underline cursor-pointer">Show</p>
+            <p className=" mt-2">{desc}</p>
+        </div>
+    )
 
-const About = ()=>{
+}
 
-    // const restaurantMenu = useRestaurantMenu("57903")
 
-    // const [restaurantGeneralInfo, restaurantAdsInfo, restaurantFoodInfo] = restaurantMenu
 
-    // console.log(restaurantGeneralInfo)
-    
-    // console.log(restaurantAdsInfo)
-    
-    // console.log(restaurantFoodInfo)
 
+const About = () => {
 
     return (
-        <>
-        <h1>About us</h1>
-        <h2>We be burning not concerning what nobody wanna say</h2>
-   
-        <Outlet />
-        </>
+        <div className="max-w-7xl flex-grow mx-auto">
+            <h1 className="text-2xl mt-5 font-bold text-center ">ABOUT THE APP</h1>
+            <div className="my-7 max-w-5xl">
+            <Section title ="About Us" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis facilis reprehenderit, quam quae quos molestiae voluptatum laudantium eveniet odit quasi fuga officiis esse ea ipsam reiciendis pariatur quisquam incidunt! Nobis." />
+            <Section title ="Support" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis facilis reprehenderit, quam quae quos molestiae voluptatum laudantium eveniet odit quasi fuga officiis esse ea ipsam reiciendis pariatur quisquam incidunt! Nobis." />
+            <Section title ="Careers" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis facilis reprehenderit, quam quae quos molestiae voluptatum laudantium eveniet odit quasi fuga officiis esse ea ipsam reiciendis pariatur quisquam incidunt! Nobis." />
+            </div>
+            <Outlet />
+        </div>
     )
 }
 
